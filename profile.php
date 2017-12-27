e993ffe35dc6d5ec47ce640a292d392c897e5be6
apostolos107 <apostolos107@gmail.com>
2017-12-27 14:54:09 +0200

    yes

--- profile.php
+++ profile.php
@@ -1,143 +1,73 @@
 <!DOCTYPE html>
 <html>
-<title>W3.CSS Template</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="./css/other.css" />
-
+<head>
+	<title>Home Page</title>
+	<meta charset="UTF-8">
+	<meta content="width=device-width, initial-scale=1" name="viewport">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">
+	</script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
+	</script>
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <!-- Latest compiled and minified CSS -->
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+	<link href="./css/other.css" rel="stylesheet">
+</head>
 <body>
-
-<!-- Navbar -->
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">ΙΚΑ</a>
-    </div>
-    <ul class="nav navbar-nav homenav">
-      <li class="active"><a href="#">Αρχική</a></li>
-    </ul>
-    <ul class="nav navbar-nav mynavbar" >
-      <li><a href="#">Αναζήτηση</a></li>
-      <li><a href="#">Γλώσσα</a></li>
-      <li><a href="#">Είσοδος/Έγγραφη</a></li>
-    </ul>
-  </div>
-</nav>
-
-<!-- Sidebar -->
-<nav class="w3-sidebar w3-bar-block w3-collapse w3-large w3-theme-l5 w3-animate-left" id="mySidebar">
-  <a href="javascript:void(0)" onclick="w3_close()" class="w3-right w3-xlarge w3-padding-large w3-hover-black w3-hide-large" title="Close Menu">
-    <i class="fa fa-remove"></i>
-  </a>
-  <h4 class="w3-bar-item"><b>Μενού</b></h4>
-  <a class="w3-bar-item w3-button w3-hover-black" href="#">Αρχική</a>
-  <a class="w3-bar-item w3-button w3-hover-black" href="#">Ασφάλεια</a>
-  <a class="w3-bar-item w3-button w3-hover-black" href="#">Σύνταξη</a>
-  <a class="w3-bar-item w3-button w3-hover-black" href="#">Επιχειρήσεις</a>
-  <a class="w3-bar-item w3-button w3-hover-black" href="#">Οφειλές</a>
-  <a class="w3-bar-item w3-button w3-hover-black" href="#">Άτομα με ειδικές Αναπηρίες</a>
-  <a class="w3-bar-item w3-button w3-hover-black" href="#">Ενημερώσεις</a>
-  <a class="w3-bar-item w3-button w3-hover-black" href="#">Συχνές Ερωτήσεις</a>
-  <a class="w3-bar-item w3-button w3-hover-black" href="#">Επικοινωνία</a>
-</nav>
-
-<!-- Overlay effect when opening sidebar on small screens -->
-<div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
-
-<!-- Main content: shift it to the right by 250 pixels when the sidebar is visible -->
-<div class="w3-main" style="margin-left:250px">
-
-  <div class="w3-row w3-padding-64">
-    <div class="w3-twothird w3-container">
-      <h1 class="w3-text-teal">Heading</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum
-        dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    </div>
-    <div class="w3-third w3-container">
-      <p class="w3-border w3-padding-large w3-padding-32 w3-center">AD</p>
-      <p class="w3-border w3-padding-large w3-padding-64 w3-center">AD</p>
-    </div>
-  </div>
-
-  <div class="w3-row">
-    <div class="w3-twothird w3-container">
-      <h1 class="w3-text-teal">Heading</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum
-        dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    </div>
-    <div class="w3-third w3-container">
-      <p class="w3-border w3-padding-large w3-padding-32 w3-center">AD</p>
-      <p class="w3-border w3-padding-large w3-padding-64 w3-center">AD</p>
-    </div>
-  </div>
-
-  <div class="w3-row w3-padding-64">
-    <div class="w3-twothird w3-container">
-      <h1 class="w3-text-teal">Heading</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum
-        dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    </div>
-    <div class="w3-third w3-container">
-      <p class="w3-border w3-padding-large w3-padding-32 w3-center">AD</p>
-      <p class="w3-border w3-padding-large w3-padding-64 w3-center">AD</p>
-    </div>
-  </div>
-
-  <!-- Pagination -->
-  <div class="w3-center w3-padding-32">
-    <div class="w3-bar">
-      <a class="w3-button w3-black" href="#">1</a>
-      <a class="w3-button w3-hover-black" href="#">2</a>
-      <a class="w3-button w3-hover-black" href="#">3</a>
-      <a class="w3-button w3-hover-black" href="#">4</a>
-      <a class="w3-button w3-hover-black" href="#">5</a>
-      <a class="w3-button w3-hover-black" href="#">»</a>
-    </div>
-  </div>
-
-  <footer id="myFooter">
-    <div class="w3-container w3-theme-l2 w3-padding-32">
-      <h4>Footer</h4>
-    </div>
-
-    <div class="w3-container w3-theme-l1">
-      <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
-    </div>
-  </footer>
-
-<!-- END MAIN -->
-</div>
-
-<script>
-// Get the Sidebar
-var mySidebar = document.getElementById("mySidebar");
-
-// Get the DIV with overlay effect
-var overlayBg = document.getElementById("myOverlay");
-
-// Toggle between showing and hiding the sidebar, and add overlay effect
-function w3_open() {
-    if (mySidebar.style.display === 'block') {
-        mySidebar.style.display = 'none';
-        overlayBg.style.display = "none";
-    } else {
-        mySidebar.style.display = 'block';
-        overlayBg.style.display = "block";
-    }
-}
-
-// Close the sidebar with the close button
-function w3_close() {
-    mySidebar.style.display = "none";
-    overlayBg.style.display = "none";
-}
-</script>
-
+	<nav class="navbar navbar-inverse">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="#">ΙΚΑ</a>
+			</div>
+			<ul class="nav navbar-nav homenav">
+				<li class="active">
+					<a href="#">Αρχική</a>
+				</li>
+			</ul>
+			<ul class="nav navbar-nav mynavbar">
+				<li>
+					<a href="#">Αναζήτηση</a>
+				</li>
+				<li>
+					<a href="#">Γλώσσα</a>
+				</li>
+				<li>
+					<a href="#">Είσοδος/Έγγραφη</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+	<nav class="main-menu">
+		<ul>
+			<li>
+				<a href="http://justinfarrow.com"><i class="fa fa-home fa-2x"></i> <span class="nav-text">Dashboard</span></a>
+			</li>
+			<li class="has-subnav">
+				<a href="#"><i class="fa fa-laptop fa-2x"></i> <span class="nav-text">UI Components</span></a>
+			</li>
+			<li class="has-subnav">
+				<a href="#"><i class="fa fa-list fa-2x"></i> <span class="nav-text">Forms</span></a>
+			</li>
+			<li class="has-subnav">
+				<a href="#"><i class="fa fa-folder-open fa-2x"></i> <span class="nav-text">Pages</span></a>
+			</li>
+			<li>
+				<a href="#"><i class="fa fa-bar-chart-o fa-2x"></i> <span class="nav-text">Graphs and Statistics</span></a>
+			</li>
+			<li>
+				<a href="#"><i class="fa fa-font fa-2x"></i> <span class="nav-text">Typography and Icons</span></a>
+			</li>
+			<li>
+				<a href="#"><i class="fa fa-table fa-2x"></i> <span class="nav-text">Tables</span></a>
+			</li>
+			<li>
+				<a href="#"><i class="fa fa-map-marker fa-2x"></i> <span class="nav-text">Maps</span></a>
+			</li>
+			<li>
+				<a href="#"><i class="fa fa-info fa-2x"></i> <span class="nav-text">Documentation</span></a>
+			</li>
+		</ul>
+	</nav>
 </body>
 </html>
